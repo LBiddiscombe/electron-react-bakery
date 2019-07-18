69cc3ea3fe5cb1a13ab0b65d826a6426aae8815b
BIDDL <lee.biddiscombe@coop.co.uk>
2019-07-18 15:35:15 +0100

    moved nav css module to styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,62 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../images/logo.svg'
 import menu from '../images/menu.svg'
-import NavDrawer from './NavDrawer'
-import styles from './Navbar.module.css'
+import styled from 'styled-components'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className={styles.navbar}>
-      <img src={logo} className={styles.logo} alt='logo' />
-      <h1 className={styles.title}>Bakery Labels</h1>
-      <button onClick={() => setIsOpen(true)} className={styles.menu}>
+    <NavBar>
+      <Link to={`/`}>
+        <Logo src={logo} alt='logo' />
+      </Link>
+      <Title>Bakery Labels</Title>
+      <Menu onClick={() => setIsOpen(true)}>
         <img src={menu} alt='menu' />
-      </button>
-      <NavDrawer isOpen={isOpen} onClick={() => setIsOpen(false)} />
-    </div>
+      </Menu>
+    </NavBar>
   )
 }
+
+const NavBar = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-areas: 'logo title menu';
+  align-items: center;
+  background-color: var(--primary-light);
+  max-height: 5rem;
+  text-align: center;
+`
+
+const Logo = styled.img`
+  grid-area: logo;
+  width: 4rem;
+  height: 4rem;
+  margin: 0.5rem;
+  -webkit-app-region: drag;
+  & :hover {
+    cursor: grab;
+  }
+`
+
+const Title = styled.h1`
+  grid-area: title;
+  font-weight: bold;
+  color: var(--primary-dark);
+  line-height: 4rem;
+  margin: 0.5rem;
+  white-space: nowrap;
+`
+
+const Menu = styled.button`
+  border: none;
+  background-color: transparent;
+  grid-area: menu;
+  width: 4rem;
+  height: 4rem;
+  margin: 0.5rem;
+`
 
 export default Navbar
