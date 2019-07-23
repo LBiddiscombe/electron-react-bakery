4a1af1c2ea0a71dbbb6f3cb4b44661007687d63c
Lee Biddiscombe (Food IT) <lee.biddiscombe@coop.co.uk>
2019-07-23 10:01:11 +0100

    added optional blur size arg

--- src/hooks/useBlurModalBG.js
+++ src/hooks/useBlurModalBG.js
@@ -1,11 +1,11 @@
 import { useState, useLayoutEffect } from 'react'
 
-export default function useBlurModalBG(parent = document.body) {
+export default function useBlurModalBG(parent = document.body, size = '4px') {
   useLayoutEffect(() => {
     // Get original body overflow
     const originalStyle = window.getComputedStyle(parent).filter
     // Prevent scrolling on mount
-    parent.style.filter = 'blur(5px)'
+    parent.style.filter = `blur(${size})`
     // Re-enable scrolling when component unmounts
     return () => (parent.style.filter = originalStyle)
   }, []) // Empty array ensures effect is only run on mount and unmount
